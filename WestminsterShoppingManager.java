43c051e25ebd9ee740192849e5c56f3e2f93ef7b
laka <jal.siriwardena@gmail.com>
2023-12-27 12:50:30 +0530

    updated the westminsterShoppingManager class

---
+++ WestminsterShoppingManager.java
@@ -0,0 +1,23 @@
+package onlineShopping;
+import java.util.*;
+
+public class WestminsterShoppingManager {
+    private ArrayList<Product> productsList;
+
+    public void addProduct(Product product){
+        productsList.add(product);
+        System.out.println("Product added:"+product.getProductName());
+    }
+
+    public void removeProduct(Product product) {
+        productsList.remove(product);
+        System.out.println("Product removed: " + product.getProductName());
+    }
+
+    public void displayProducts() {
+        System.out.println("Products in the system:");
+        for (Product product : productsList) {
+            System.out.println(product.getProductName() + " - $" + product.getPrice() + " - Qty" + product.getAvailableItems());
+        }
+    }
+}
